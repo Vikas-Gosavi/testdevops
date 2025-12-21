a6afd7c3bdce3e1497f7225aab43ecf3957ef243
Vikas-Gosavi <vgosavi242@gmail.com>
2025-12-21 23:20:19 +0530

    Update6 test.py
    test fail build

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print ("Hi my devops testing demo pratical and add new file done")
 print ("my new 1st line to adding")
 print ("my new 2nd line to adding")
-print ("my new 3rd line to adding")
+print (my new 3rd line to adding")
